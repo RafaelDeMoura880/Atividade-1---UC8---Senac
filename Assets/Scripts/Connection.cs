56ae95c22f805d35add5c4164da4b534b767f4c1
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-02-06 18:56:11 -0300

    Adding basic features

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Connection : MonoBehaviourPunCallbacks
+{
+    string version = "1";
+    bool isConnecting = false;
+    GameObject connectingTextObj;
+    GameObject connectButtonObj;
+    
+    private void Start()
+    {
+        connectButtonObj = GameObject.Find("Canvas").transform.GetChild(0).gameObject;
+        connectingTextObj = GameObject.Find("Canvas").transform.GetChild(1).gameObject;
+    }
+
+    public void Connect()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = version;
+        }
+        else
+            PhotonNetwork.JoinRandomRoom();
+
+        connectingTextObj.gameObject.SetActive(true);
+        connectButtonObj.gameObject.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        if (isConnecting)
+        {
+            connectingTextObj.gameObject.GetComponent<Text>().text = "Connected!";
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected. Cause: " + cause);
+        connectingTextObj.gameObject.SetActive(false);
+        connectButtonObj.gameObject.SetActive(true);
+        isConnecting = false;
+    }
+}
