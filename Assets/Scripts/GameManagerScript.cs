2bff899e759619dd49e204ece91286438bba9471
Rafael de Moura Pernas <70914482+RafaelDeMoura880@users.noreply.github.com>
2022-02-06 19:27:52 -0300

    Added player

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -6,6 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviourPunCallbacks
 {
+    public GameObject playerPrefab;
+
+    private void Start()
+    {
+        if(playerPrefab != null)
+        {
+            Vector3 position = new Vector3(0, 0, 0);
+            PhotonNetwork.Instantiate(playerPrefab.name, position, Quaternion.identity);
+        }
+    }
+
     public void LeaveRoom()
     {
         PhotonNetwork.LeaveRoom();
